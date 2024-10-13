4e1b745f2521941473cb0f08cc41e145f54eb951
Namaku Wahyu <wahyu.220117rpl0205@gmail.com>
2024-10-13 22:26:27 +0700

    lupa

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// top-14 absolute border w-full left-0 divide-y-2
+//      
+
+const menu = document.querySelector('.menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+menu.addEventListener('click', displayMenu);
+hamburgerMenu.addEventListener('click', displayMenu);
+
+const iconBars = document.querySelector('.fa-bars');
+const iconClose = document.querySelector('.fa-x');
+
+function displayMenu(){
+    if (menu.classList.contains('absolute')) {
+        menu.classList.add('hidden')
+        iconBars.style.display = 'inline';
+        iconClose.style.display = 'none';
+        
+        
+        menu.classList.remove('absolute')
+        menu.classList.remove('top-20')
+        menu.classList.remove('border')
+        menu.classList.remove('w-full')
+        menu.classList.remove('left-0')
+        menu.classList.remove('divide-y-2')
+    } else {
+        menu.classList.remove('hidden')
+        iconBars.style.display = 'none';
+        iconClose.style.display = 'inline';
+        menu.classList.add('absolute')
+        menu.classList.add('top-28')
+        menu.classList.add('border')
+        menu.classList.add('w-full')
+        menu.classList.add('bg-black')
+
+    }
+}
+
