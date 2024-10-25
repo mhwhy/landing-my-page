a327722870707a8001d7fa3444085e5aa187866e
Namaku Wahyu <wahyu.220117rpl0205@gmail.com>
2024-10-25 23:14:34 +0700

    change largw

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-//      
-
-const menu = document.querySelector('.menu');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-
-menu.addEventListener('click', displayMenu);
-hamburgerMenu.addEventListener('click', displayMenu);
-
-const iconBars = document.querySelector('.fa-bars');
-const iconClose = document.querySelector('.fa-x');
-
-function displayMenu(){
-    if (menu.classList.contains('absolute')) {
-        menu.classList.add('hidden')
-        iconBars.style.display = 'inline';
-        iconClose.style.display = 'none';
-        
-        
-        menu.classList.remove('absolute')
-        menu.classList.remove('top-20')
-        menu.classList.remove('border')
-        menu.classList.remove('w-full')
-        menu.classList.remove('left-0')
-        menu.classList.remove('divide-y-2')
+document.getElementById('menu-btn').addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
     } else {
-        menu.classList.remove('hidden')
-        iconBars.style.display = 'none';
-        iconClose.style.display = 'inline';
-        menu.classList.add('absolute')
-        menu.classList.add('top-28')
-        menu.classList.add('border')
-        menu.classList.add('w-full')
-        menu.classList.add('bg-black')
-
+        menu.classList.add('hidden');
     }
+});
+
+// let currentIndex = 0;
+// const slider = document.querySelector('.slider');
+// const items = document.querySelectorAll('.slider-item');
+
+// setInterval(() => {
+//     currentIndex = (currentIndex + 1) % items.length;
+//     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }, 1110);
+
+function toggleInfoBox(element) {
+    element.classList.toggle('active');
 }
 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
